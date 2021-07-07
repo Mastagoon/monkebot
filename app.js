6b7616136f0b33fe5d10f7fd8d2b5b9a6587453b
Mastagoon <iznilagiway@gmail.com>
2021-07-07 14:55:20 +0300

    typo

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ bot.launch()
 //we're gonna use the text event instead of bot.command. to support arabic commands.
 bot.on(`text`, async ctx => {
     const text = ctx.message.text
-    if(commandList.split(" ").includes(text)) {
-        getGif()
+    if(commandList.includes(text)) {
+        getGif(ctx)
     }
 })
 
-const getGif = async() => {
+const getGif = async(ctx) => {
     const gif = await axios.get(`https://g.tenor.com/v1/search?q=monkey&key=${process.env.API_KEY}&limit=1&pos=${Math.floor(Math.random()*500)}`)
     if(gif?.data?.results?.length) return ctx.telegram.sendAnimation(ctx.chat.id, gif.data.results[0].url)
     return getGif()
