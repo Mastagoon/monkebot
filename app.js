4821d46dc3e7e8feb2be2bb2616738abf6dcb537
Mastagoon <iznilagiway@gmail.com>
2021-07-07 14:48:42 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const { Telegraf } = require('telegraf')
+const axios = require("axios")
+require("dotenv").config()
+
+const commandList = ["monkey", "monke", "monkes", "monkeys"]
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.launch()
+
+//we're gonna use the text event instead of bot.command. to support arabic commands.
+bot.on(`text`, async ctx => {
+    const text = ctx.message.text
+    if(commandList.includes(text)) {
+        try {
+            const gif = await axios.get(`https://g.tenor.com/v1/search?q=monkey&key=${process.env.API_KEY}&limit=1&pos=${Math.floor(Math.random()*500)}`)
+            ctx.telegram.sendAnimation(ctx.chat.id, gif.data.results[0].url)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+})
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
